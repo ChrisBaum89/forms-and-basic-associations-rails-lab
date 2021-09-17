e30f93a650784dd451bd4b865712796538ae31ff
Chris Baum <Christopher.Baum@valpo.edu>
2021-09-16 20:35:06 -0400

    commit tree a30c33f3611ba3927c04ec8d001aab14ee93adac

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,44 @@
 class Song < ActiveRecord::Base
-  # add associations here
-end
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    if self.genre
+      self.genre
+    else
+      nil
+    end
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != "" || content.strip != nil
+        note_content = self.notes.new(content: content)
+        note_content.song_id = self.id
+        note_content.save
+      end
+    end
+  end
+
+  def note_contents
+    notes.map{|note| note.content}
+  end
+
+  end
